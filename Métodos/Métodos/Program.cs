b1a7a89ac21e585464a6ccfed970f61448e31952
Francisco Abrantes <58854070+FranciscoAbrantes@users.noreply.github.com>
2022-11-11 12:17:22 +0000

    Add files via upload

---
+++ Métodos/Métodos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JogoBenfica("Brugge", 2);
+            JogoBenfica("Liverpool", 3);
+            JogoBenfica("Real Madrid", 4);
+
+            Console.ReadLine();
+        }
+
+        static void JogoBenfica(string equipa, int pontos)
+        {
+            Console.WriteLine("O Benfica vai jogar contra o " + equipa + "e tem " + pontos + " de avanço");
+        }
+    }
+}
---
+++ Returns/Returns/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Returns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube2(5);
+            Console.WriteLine(cubedNumber);
+            //Console.WriteLine(cube5));
+            Console.ReadLine();
+        }
+
+        /*static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }*/
+        static int cube2(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
+
