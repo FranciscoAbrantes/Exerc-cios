8c3dc28c916b6de3d8e4c589b92da1e680f3feb3
Francisco Abrantes <58854070+FranciscoAbrantes@users.noreply.github.com>
2022-11-15 12:19:18 +0000

    Add files via upload

---
+++ PedidoDeMétodo/PedidoDeMétodo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidoDeMétodo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conversão dos Int
+            Console.Write("Digite um numero: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite outro número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite mais um número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            // Enviar para o Metodo
+            Console.WriteLine(GetMax(num1, num2, num3));
+
+            // ReadLine
+            Console.ReadLine();
+        }
+
+        // Metodo
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+            return result;
+        }
+    }
+}
