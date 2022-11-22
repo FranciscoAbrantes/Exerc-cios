f72a7149e13080acbd55b2987cdadf06907aebf4
Francisco Abrantes <58854070+FranciscoAbrantes@users.noreply.github.com>
2022-11-22 12:04:31 +0000

    Add files via upload

---
+++ Ciclos/Ciclos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1; //6
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            /*int index = 6;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);*/
+            Console.ReadLine();
+        }
+    }
+}
