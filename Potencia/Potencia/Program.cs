4bdb69c82f57f00910a4013d5f64969f808c3f06
Francisco Abrantes <58854070+FranciscoAbrantes@users.noreply.github.com>
2022-12-09 10:10:53 +0000

    Add files via upload

---
+++ Potencia/Potencia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPower(4, 3));
+            Console.ReadLine();
+        }
+
+        static int GetPower(int bas, int expoent)
+        {
+            int i;
+            int result = 1;
+
+            for (i = 0; i < expoent; i++)
+            {
+                result = result * bas;
+            }
+            return result;
+        }
+    }
+}
